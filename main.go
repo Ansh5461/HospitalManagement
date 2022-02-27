cb583d517d2fe1e2f32230626b88c807de3c2afa
Ansh5461 <anshjoshi0607@gmail.com>
2022-02-27 22:28:56 +0530

    Done with main function

--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"log"
+
 	"github.com/Ansh5461/HospitalManagement/database"
 	"github.com/Ansh5461/HospitalManagement/handler"
 	"github.com/Ansh5461/HospitalManagement/router"
@@ -8,11 +10,16 @@ import (
 )
 
 func main() {
-	engine := gin.Default()
 
+	engine := gin.Default() //get the default engine for further customizatikon
 	api := handler.Handler{
-		DB: database.GetDB(), //set the handler DB here in main
-	}
+		DB: database.GetDB(), //set the handler DB
+	} //get the customized engine
 
-	router.SetupRouter()
+	database.Setup()
+	router.SetupRouter(engine, api)
+	err := engine.Run("127.0.0.1:8080")
+	if err != nil {
+		log.Fatal(err)
+	}
 }
