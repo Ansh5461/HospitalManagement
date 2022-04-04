84b3884a566b931a3c069e494b8ae1eb01a65bc0
Ansh5461 <anshjoshi0607@gmail.com>
2022-04-04 11:06:04 +0530

    Trying to resolve errors

--- main.go
+++ main.go
@@ -4,22 +4,24 @@ import (
 	"log"
 
 	"github.com/Ansh5461/HospitalManagement/database"
-	"github.com/Ansh5461/HospitalManagement/handler"
+	//	"github.com/Ansh5461/HospitalManagement/handler"
 	"github.com/Ansh5461/HospitalManagement/router"
-	"github.com/gin-gonic/gin"
 )
 
 func main() {
 
-	engine := gin.Default() //get the default engine for further customizatikon
-	api := handler.Handler{
-		DB: database.GetDB(), //set the handler DB
-	} //get the customized engine
-
 	database.Setup()
-	router.SetupRouter(engine, api)
+	engine := router.Router() //get the default engine for further customizatikon
 	err := engine.Run("127.0.0.1:8080")
 	if err != nil {
 		log.Fatal(err)
 	}
+
+	//	api := handler.Handler{
+	//		DB: database.GetDB(), //set the handler DB
+	//	} //get the customized engine
+
+	//router.SetupRouter(engine, api)
+	//err := engine.Run("127.0.0.1:8080")
+
 }
--- router/hospital.go
+++ router/hospital.go
@@ -3,6 +3,7 @@ package router
 import (
 	"github.com/gin-gonic/gin"
 
+	"github.com/Ansh5461/HospitalManagement/database"
 	"github.com/Ansh5461/HospitalManagement/handler"
 )
 
@@ -15,3 +16,29 @@ func SetupRouter(router *gin.Engine, api handler.Handler) {
 	router.DELETE("/hospital/:patient_id", api.DeletePatientByID)
 
 }
+
+func Router() *gin.Engine {
+	r := gin.Default()
+
+	api := handler.Handler{
+		DB: database.GetDB(),
+	}
+	r.GET("/hospital", api.GetPatient)
+	r.POST("/hospital", api.SavePatient)
+	r.GET("/hospital/:patient_id", api.DeletePatientByID)
+	//router.GET("/hospital/:patient_name", api.GetPatientRoom)
+	r.DELETE("/hospital/:patient_id", api.DeletePatientByID)
+
+	return r
+}
+
+//{
+//    "patient_name":"Ansh",
+//	"patient_id": "C123",
+//	"room_no": 123,
+//	"disease":"Dil mei ched"
+//}
+//
+//
+
+//video 11 59:59
