d432b6e0e813a3ce71bd6b9b5e10a4dd03691a6f
Ansh5461 <anshjoshi0607@gmail.com>
2022-04-09 02:33:06 +0530

    Possibly complete

--- models/hospital.go
+++ models/hospital.go
@@ -4,8 +4,8 @@ import "github.com/jinzhu/gorm"
 
 type Hospital struct {
 	gorm.Model
-	patient_name string
-	patient_id   string
-	room_no      int
-	disease      string
+	Patient_name string
+	Patient_id   string
+	Room_no      int
+	Disease      string
 }
